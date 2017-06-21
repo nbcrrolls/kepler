c39443f075ba14606a3c8cb7866b884809c4a739
root <root@rocce-vm2.ucsd.edu>
2017-06-20 17:43:05 -0700

    use biokepler-1.2

--- src/kepler/version.mk
+++ src/kepler/version.mk
@@ -1,10 +1,10 @@
 PKGROOT		= /opt
 NAME    	= kepler
-VERSION 	= 2.5
-DISTNAME        = Kepler-$(VERSION)
+VERSION 	= 1.2
+DISTNAME        = biokepler-$(VERSION)
 RELEASE 	= 0
 TARBALL_POSTFIX	= tar.gz
 
-SOURCE_PKG      = Kepler-$(VERSION)-linux.$(TARBALL_POSTFIX)
+SOURCE_PKG      = biokepler-$(VERSION).$(TARBALL_POSTFIX)
 
 RPM.EXTRAS  = %define __os_install_post /usr/lib/rpm/brp-compress \\n%define __strip /bin/false
